582bcff3ee7f17adc3ab66e8c42915d7fff41a7e
AzureAD\JasonLau <jason.lau@varmeego.com>
2023-02-22 10:23:48 +0800

    remove constructor

--- src/Components/LoadingScreen/LoadingScreen.js
+++ src/Components/LoadingScreen/LoadingScreen.js
@@ -2,9 +2,6 @@ import React from "react";
 import './LoadingScreen.css';
 
 export class LoadingScreen extends React.Component {
-    constructor(props) {
-        super(props);
-    }
 
     render() {
         return(
